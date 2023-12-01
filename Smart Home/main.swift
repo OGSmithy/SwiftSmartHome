61b5190115917cd37e8fce6692d0c5275073ae21
Oli Smith <smith.o@etoncollege.org.uk>
2023-12-01 11:21:34 +0000

    Changeable

--- Smart Home/main.swift
+++ Smart Home/main.swift
@@ -12,14 +12,19 @@ class SmartHome {
     private var devices = [Devices]()
     
     func getDevicesStatuses() {
-        
+        print([Devices].self)
     }
-    
+    func turnOnLights() {
+        Lights(isOn: true, colour: "Blue", type: "LED")
+    }
+    func changeHeatersIntensity(newIntensity: Int) {
+        Heaters(isOn: true, type: "Log Fire", intensity: newIntensity)
+    }
 }
 
 class Devices: SmartHome {
     private var isOn: Bool
-    private var type: String
+    private let type: String
     
     init (isOn: Bool, type: String) {
         self.isOn = isOn
@@ -46,3 +51,29 @@ class Heaters: Devices {
     
 }
 
+let mySmartHome = SmartHome()
+mySmartHome.getDevicesStatuses()
+
+print("""
+
+    What would you like to do?
+    1 : Toggle lights
+    2 : Change Heater Intensity
+p
+""")
+
+if let userChoice = readLine() {
+    switch userChoice {
+    case "1":
+        print("The lights have been toggled")
+    case "2":
+        print("What intensity would you like? (0-10) ")
+        if let newIntensityStr = readLine() {
+            if let newIntensity = Int(newIntensityStr) {
+                print("The heaters no have an intensity of \(mySmartHome.changeHeatersIntensity(newIntensity: newIntensity))")
+            }
+        }
+    default:
+        print("You did nothing")
+        }
+}
